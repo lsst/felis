038305560782d6d2ae16442f1631c813122d5484
Brian Van Klaveren <bvan@slac.stanford.edu>
2018-11-30 11:30:35 -0800

    Add graph framing and merging

--- felis/cli.py
+++ felis/cli.py
@@ -17,14 +17,17 @@
 # You should have received a copy of the LSST License Statement and
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
+import json
 
 import click
 import yaml
+from pyld import jsonld
 from sqlalchemy import create_engine
 
 from .model import Visitor
 from .tap import TapLoadingVisitor, Tap11Base
-from . import __version__
+from .utils import ReorderingVisitor
+from . import __version__, DEFAULT_CONTEXT, DEFAULT_FRAME
 
 
 @click.group()
@@ -103,40 +106,60 @@ def load_tap(engine_url, schema_name, catalog_name, dry_run, file):
     tap_visitor.visit_schema(schema_obj)
 
 
-# @cli.command("normalize")
-# @click.argument('file', type=click.File())
-# def normalize(file):
-#     """Normalize a Felis FILE.
-#     This command loads the associated TAP metadata from a Felis FILE
-#     to the TAP_SCHEMA tables."""
-#     schema_obj = yaml.load(file)
-#     schema_obj["@type"] = "felis:Schema"
-#     # Force Context and Schema Type
-#     schema_obj["@context"] = {
-#         "@vocab": "http://lsst.org/felis/",
-#         "mysql": "http://mysql.com/",
-#         "postgres": "http://posgresql.org/",
-#         "oracle": "http://oracle.com/database/",
-#         "sqlite": "http://sqlite.org/",
-#         "fits": "http://fits.gsfc.nasa.gov/FITS/4.0/",
-#         "ivoa": "http://ivoa.net/rdf/",
-#         "votable": "http://ivoa.net/rdf/VOTable/",
-#         "tap": "http://ivoa.net/documents/TAP/",
-#         "tables": {
-#             "@id": "felis:Table",
-#             "@container": "@list"
-#         },
-#         "columns": {
-#             "@id": "felis:Column",
-#             "@container": "@list"
-#         }
-#     }
-#     json.dump(schema_obj, open("output.jsonld", "w"), indent=4, sort_keys=True)
-#     from pyld import jsonld
-#     framed = jsonld.frame(schema_obj, DEFAULT_FRAME)
-#     print(json.dumps(framed, indent=2))
-#     compacted = jsonld.compact(framed, DEFAULT_CONTEXT)
-#     print(json.dumps(compacted, indent=2))
+@cli.command("normalize")
+@click.argument('file', type=click.File())
+def normalize(file):
+    """Normalize a Felis FILE."""
+    schema_obj = yaml.load(file)
+    schema_obj["@type"] = "felis:Schema"
+    # Force Context and Schema Type
+    schema_obj["@context"] = DEFAULT_CONTEXT
+    expanded = jsonld.expand(schema_obj)
+    normalized = _normalize(expanded)
+    _dump(normalized)
+
+
+@cli.command("merge")
+@click.argument('files', nargs=-1, type=click.File())
+def merge(files):
+    """Merge a set of Feils FILES"""
+    graph = []
+    for file in files:
+        schema_obj = yaml.load(file)
+        schema_obj["@type"] = "felis:Schema"
+        # Force Context and Schema Type
+        schema_obj["@context"] = DEFAULT_CONTEXT
+        expanded = jsonld.expand(schema_obj)
+        graph.extend(expanded)
+
+    merged = {"@context": DEFAULT_CONTEXT,
+              "@graph": graph}
+    normalized = _normalize(merged)
+    _dump(normalized)
+
+
+def _dump(obj):
+    class OrderedDumper(yaml.Dumper):
+        pass
+
+    def _dict_representer(dumper, data):
+        return dumper.represent_mapping(
+            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+            data.items())
+
+    OrderedDumper.add_representer(dict, _dict_representer)
+    print(yaml.dump(obj, Dumper=OrderedDumper, default_flow_style=False))
+
+
+def _normalize(schema_obj):
+    framed = jsonld.frame(schema_obj, DEFAULT_FRAME)
+    compacted = jsonld.compact(framed, DEFAULT_CONTEXT)
+    graph = compacted["@graph"]
+    if not isinstance(graph, list):
+        graph = [graph]
+    graph = [ReorderingVisitor(add_type=True).visit_schema(schema_obj) for schema_obj in graph]
+    compacted["@graph"] = graph if len(graph) > 1 else graph[0]
+    return compacted
 
 
 class InsertDump:
