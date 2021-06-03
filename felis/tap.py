45cf6905c0f451e1146b47e772883681f398588e
Brian Van Klaveren <bvan@slac.stanford.edu>
2021-06-02 21:37:21 -0700

    Add support for tap-x-table options Reformat with black (120) Improve command documentation

--- felis/tap.py
+++ felis/tap.py
@@ -1,8 +1,6 @@
-from typing import List
-
 import logging
 
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Table
 from sqlalchemy.engine import Engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, sessionmaker
@@ -21,76 +19,88 @@ TEXT_FIELD_LENGTH = 2048
 QUALIFIED_TABLE_LENGTH = 3 * IDENTIFIER_LENGTH + 2
 
 
-class Tap11Schemas(Tap11Base):
-    __tablename__ = "schemas"
-    schema_name = Column(String(IDENTIFIER_LENGTH), primary_key=True, nullable=False)
-    utype = Column(String(SIMPLE_FIELD_LENGTH))
-    description = Column(String(TEXT_FIELD_LENGTH))
-    schema_index = Column(Integer)
+def init_tables(
+    tap_schema_name=None,
+    tap_schemas_table=None,
+    tap_tables_table=None,
+    tap_columns_table=None,
+    tap_keys_table=None,
+    tap_key_columns_table=None,
+):
+    if tap_schema_name:
+        Tap11Base.metadata.schema = tap_schema_name
 
+    class Tap11Schemas(Tap11Base):
+        __tablename__ = tap_schemas_table or "schemas"
+        schema_name = Column(String(IDENTIFIER_LENGTH), primary_key=True, nullable=False)
+        utype = Column(String(SIMPLE_FIELD_LENGTH))
+        description = Column(String(TEXT_FIELD_LENGTH))
+        schema_index = Column(Integer)
 
-class Tap11Tables(Tap11Base):
-    __tablename__ = "tables"
-    schema_name = Column(String(IDENTIFIER_LENGTH), nullable=False)
-    table_name = Column(String(QUALIFIED_TABLE_LENGTH), nullable=False, primary_key=True)
-    table_type = Column(String(SMALL_FIELD_LENGTH), nullable=False)
-    utype = Column(String(SIMPLE_FIELD_LENGTH))
-    description = Column(String(TEXT_FIELD_LENGTH))
-    table_index = Column(Integer)
+    class Tap11Tables(Tap11Base):
+        __tablename__ = tap_tables_table or "tables"
+        schema_name = Column(String(IDENTIFIER_LENGTH), nullable=False)
+        table_name = Column(String(QUALIFIED_TABLE_LENGTH), nullable=False, primary_key=True)
+        table_type = Column(String(SMALL_FIELD_LENGTH), nullable=False)
+        utype = Column(String(SIMPLE_FIELD_LENGTH))
+        description = Column(String(TEXT_FIELD_LENGTH))
+        table_index = Column(Integer)
 
+    class Tap11Columns(Tap11Base):
+        __tablename__ = tap_columns_table or "columns"
+        table_name = Column(String(QUALIFIED_TABLE_LENGTH), nullable=False, primary_key=True)
+        column_name = Column(String(IDENTIFIER_LENGTH), nullable=False, primary_key=True)
+        datatype = Column(String(SIMPLE_FIELD_LENGTH), nullable=False)
+        arraysize = Column(String(10))
+        xtype = Column(String(SIMPLE_FIELD_LENGTH))
+        # Size is deprecated
+        # size = Column(Integer(), quote=True)
+        description = Column(String(TEXT_FIELD_LENGTH))
+        utype = Column(String(SIMPLE_FIELD_LENGTH))
+        unit = Column(String(SIMPLE_FIELD_LENGTH))
+        ucd = Column(String(SIMPLE_FIELD_LENGTH))
+        indexed = Column(Integer, nullable=False)
+        principal = Column(Integer, nullable=False)
+        std = Column(Integer, nullable=False)
+        column_index = Column(Integer)
 
-class Tap11Columns(Tap11Base):
-    __tablename__ = "columns"
-    table_name = Column(String(QUALIFIED_TABLE_LENGTH), nullable=False, primary_key=True)
-    column_name = Column(String(IDENTIFIER_LENGTH), nullable=False, primary_key=True)
-    datatype = Column(String(SIMPLE_FIELD_LENGTH), nullable=False)
-    arraysize = Column(String(10))
-    xtype = Column(String(SIMPLE_FIELD_LENGTH))
-    # Size is deprecated
-    # size = Column(Integer(), quote=True)
-    description = Column(String(TEXT_FIELD_LENGTH))
-    utype = Column(String(SIMPLE_FIELD_LENGTH))
-    unit = Column(String(SIMPLE_FIELD_LENGTH))
-    ucd = Column(String(SIMPLE_FIELD_LENGTH))
-    indexed = Column(Integer, nullable=False)
-    principal = Column(Integer, nullable=False)
-    std = Column(Integer, nullable=False)
-    column_index = Column(Integer)
+    class Tap11Keys(Tap11Base):
+        __tablename__ = tap_keys_table or "keys"
+        key_id = Column(String(IDENTIFIER_LENGTH), nullable=False, primary_key=True)
+        from_table = Column(String(QUALIFIED_TABLE_LENGTH), nullable=False)
+        target_table = Column(String(QUALIFIED_TABLE_LENGTH), nullable=False)
+        description = Column(String(TEXT_FIELD_LENGTH))
+        utype = Column(String(SIMPLE_FIELD_LENGTH))
 
+    class Tap11KeyColumns(Tap11Base):
+        __tablename__ = tap_key_columns_table or "key_columns"
+        key_id = Column(String(IDENTIFIER_LENGTH), nullable=False, primary_key=True)
+        from_column = Column(String(IDENTIFIER_LENGTH), nullable=False, primary_key=True)
+        target_column = Column(String(IDENTIFIER_LENGTH), nullable=False, primary_key=True)
 
-class Tap11Keys(Tap11Base):
-    __tablename__ = "keys"
-    key_id = Column(String(IDENTIFIER_LENGTH), nullable=False, primary_key=True)
-    from_table = Column(String(QUALIFIED_TABLE_LENGTH), nullable=False)
-    target_table = Column(String(QUALIFIED_TABLE_LENGTH), nullable=False)
-    description = Column(String(TEXT_FIELD_LENGTH))
-    utype = Column(String(SIMPLE_FIELD_LENGTH))
-
-
-class Tap11KeyColumns(Tap11Base):
-    __tablename__ = "key_columns"
-    key_id = Column(String(IDENTIFIER_LENGTH), nullable=False, primary_key=True)
-    from_column = Column(String(IDENTIFIER_LENGTH), nullable=False, primary_key=True)
-    target_column = Column(String(IDENTIFIER_LENGTH), nullable=False, primary_key=True)
+    return dict(
+        schemas=Tap11Schemas, tables=Tap11Tables, columns=Tap11Columns, keys=Tap11Keys, key_columns=Tap11KeyColumns
+    )
 
 
 class TapLoadingVisitor(VisitorBase):
-    def __init__(self, engine: Engine, catalog_name=None, schema_name=None, mock=False):
+    def __init__(self, engine: Engine, catalog_name=None, schema_name=None, mock=False, tap_tables=None):
         self.graph_index = {}
         self.catalog_name = catalog_name
         self.schema_name = schema_name
         self.engine = engine
         self.mock = mock
+        self.tables = tap_tables or init_tables()
 
     def visit_schema(self, schema_obj):
-        schema = Tap11Schemas()
+        schema = self.tables["schemas"]
         # Override with default
         self.schema_name = self.schema_name or schema_obj["name"]
 
         schema.schema_name = self._schema_name()
         schema.description = schema_obj.get("description")
         schema.utype = schema_obj.get("votable:utype")
-        schema.schema_index = int(schema_obj.get("tap:schema_index",0))
+        schema.schema_index = int(schema_obj.get("tap:schema_index", 0))
 
         if not self.mock:
             session: Session = sessionmaker(self.engine)()
@@ -105,21 +115,21 @@ class TapLoadingVisitor(VisitorBase):
         else:
             # Only if we are mocking (dry run)
             conn = self.engine
-            conn.execute(_insert(Tap11Schemas, schema))
+            conn.execute(_insert(self.tables["schemas"], schema))
             for table_obj in schema_obj["tables"]:
                 table, columns, keys, key_columns = self.visit_table(table_obj, schema_obj)
-                conn.execute(_insert(Tap11Tables, table))
+                conn.execute(_insert(self.tables["tables"], table))
                 for column in columns:
-                    conn.execute(_insert(Tap11Columns, column))
+                    conn.execute(_insert(self.tables["columns"], column))
                 for key in keys:
-                    conn.execute(_insert(Tap11Keys, key))
+                    conn.execute(_insert(self.tables["keys"], key))
                 for key_column in key_columns:
-                    conn.execute(_insert(Tap11KeyColumns, key_column))
+                    conn.execute(_insert(self.tables["key_columns"], key_column))
 
     def visit_table(self, table_obj, schema_obj):
         self.check_table(table_obj, schema_obj)
         table_id = table_obj["@id"]
-        table = Tap11Tables()
+        table = self.tables["tables"]()
         table.schema_name = self._schema_name()
         table.table_name = self._table_name(table_obj["name"])
         table.table_type = "table"
@@ -152,24 +162,28 @@ class TapLoadingVisitor(VisitorBase):
             # It is expected that both arraysize and length are fine for length types.
             arraysize = column_obj.get("votable:arraysize", column_obj.get("length"))
             if arraysize is None:
-                logger.warning(f"votable:arraysize and length for {_id} are None for type {datatype_name}. "
-                               "Using length \"*\". "
-                               "Consider setting `votable:arraysize` or `length`.")
+                logger.warning(
+                    f"votable:arraysize and length for {_id} are None for type {datatype_name}. "
+                    'Using length "*". '
+                    "Consider setting `votable:arraysize` or `length`."
+                )
         if datatype_name in DATETIME_TYPES:
             # datetime types really should have a votable:arraysize, because they are converted
             # to strings and the `length` is loosely to the string size
             if "votable:arraysize" not in column_obj:
-                logger.warning(f"votable:arraysize for {_id} is None for type {datatype_name}. "
-                               f"Using length \"*\". "
-                               "Consider setting `votable:arraysize` to an appropriate size for "
-                               "materialized datetime/timestamp strings.")
+                logger.warning(
+                    f"votable:arraysize for {_id} is None for type {datatype_name}. "
+                    f'Using length "*". '
+                    "Consider setting `votable:arraysize` to an appropriate size for "
+                    "materialized datetime/timestamp strings."
+                )
 
     def visit_column(self, column_obj, table_obj):
         self.check_column(column_obj, table_obj)
         column_id = column_obj["@id"]
         table_name = self._table_name(table_obj["name"])
 
-        column = Tap11Columns()
+        column = self.tables["columns"]()
         column.table_name = table_name
         column.column_name = column_obj["name"]
 
@@ -207,9 +221,7 @@ class TapLoadingVisitor(VisitorBase):
         if primary_key_obj:
             if not isinstance(primary_key_obj, list):
                 primary_key_obj = [primary_key_obj]
-            columns = [
-                self.graph_index[c_id] for c_id in primary_key_obj
-            ]
+            columns = [self.graph_index[c_id] for c_id in primary_key_obj]
             # if just one column and it's indexed, update the object
             if len(columns) == 1:
                 columns[0].indexed = 1
@@ -225,12 +237,8 @@ class TapLoadingVisitor(VisitorBase):
             description = constraint_obj.get("description")
             utype = constraint_obj.get("votable:utype")
 
-            columns: List[Tap11Columns] = [
-                self.graph_index[c_id] for c_id in constraint_obj.get("columns", [])
-            ]
-            refcolumns: List[Tap11Columns] = [
-                self.graph_index[c_id] for c_id in constraint_obj.get("referencedColumns", [])
-            ]
+            columns = [self.graph_index[c_id] for c_id in constraint_obj.get("columns", [])]
+            refcolumns = [self.graph_index[c_id] for c_id in constraint_obj.get("referencedColumns", [])]
 
             table_name = None
             for column in columns:
@@ -248,14 +256,14 @@ class TapLoadingVisitor(VisitorBase):
             first_column = columns[0]
             first_refcolumn = refcolumns[0]
 
-            key = Tap11Keys()
+            key = self.tables["keys"]()
             key.key_id = constraint_name
             key.from_table = first_column.table_name
             key.target_table = first_refcolumn.table_name
             key.description = description
             key.utype = utype
             for column, refcolumn in zip(columns, refcolumns):
-                key_column = Tap11KeyColumns()
+                key_column = self.tables["key_columns"]()
                 key_column.key_id = constraint_name
                 key_column.from_column = column.column_name
                 key_column.target_column = refcolumn.column_name
@@ -264,9 +272,7 @@ class TapLoadingVisitor(VisitorBase):
 
     def visit_index(self, index_obj, table_obj):
         self.check_index(index_obj, table_obj)
-        columns = [
-            self.graph_index[c_id] for c_id in index_obj.get("columns", [])
-        ]
+        columns = [self.graph_index[c_id] for c_id in index_obj.get("columns", [])]
         # if just one column and it's indexed, update the object
         if len(columns) == 1:
             columns[0].indexed = 1
