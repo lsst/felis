43935ccc4aa2429672c29f5773572a138a72824e
Jeremy McCormick <jeremym@slac.stanford.edu>
2024-03-18 16:52:06 -0500

    Correct cli handling for sqlite and implement automatic dry run
    Instead of not setting the schema name, use "main" instead which works
    for sqlite. In case a host name is not set on the database engine URL,
    force a dry run using the supplied variant.

--- python/felis/cli.py
+++ python/felis/cli.py
@@ -101,13 +101,18 @@ def create(
     """Create database objects from the Felis file."""
     yaml_data = yaml.safe_load(file)
     schema = Schema.model_validate(yaml_data)
+    url_obj = make_url(engine_url)
     if schema_name:
         logger.info(f"Overriding schema name with: {schema_name}")
         schema.name = schema_name
+    elif url_obj.drivername == "sqlite":
+        logger.info("Overriding schema name for sqlite with: main")
+        schema.name = "main"
+    if not url_obj.host and not url_obj.drivername == "sqlite":
+        dry_run = True
+        logger.info("Forcing dry run for non-sqlite engine URL with no host")
 
-    url_obj = make_url(engine_url)
-
-    builder = MetaDataBuilder(schema, apply_schema_name=True if url_obj.drivername == "sqlite" else False)
+    builder = MetaDataBuilder(schema)
     builder.build()
     metadata = builder.metadata
     logger.debug(f"Created metadata with schema name: {metadata.schema}")
@@ -131,7 +136,7 @@ def create(
 
     if create_if_not_exists:
         logger.debug("Creating schema if not exists")
-        context.create_if_not_exists(engine)
+        context.create_if_not_exists()
 
     context.create_all()
 
