9c22f6c4bec7993aee1dd317f09b82c1ee275a2a
Jeremy McCormick <jeremym@slac.stanford.edu>
2024-03-19 15:35:30 -0500

    Add tests for indexes and constraints in MetaData test case
    Test that the metadata from the builder and that reflected from the
    database have equivalent index and constraint objects. Add some
    additional constraints to the YAML test file.

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -23,7 +23,16 @@ import os
 import unittest
 
 import yaml
-from sqlalchemy import MetaData, create_engine
+from sqlalchemy import (
+    CheckConstraint,
+    Constraint,
+    ForeignKeyConstraint,
+    Index,
+    MetaData,
+    PrimaryKeyConstraint,
+    UniqueConstraint,
+    create_engine,
+)
 
 from felis.datamodel import Schema
 from felis.metadata import DatabaseContext, MetaDataBuilder
@@ -46,11 +55,25 @@ class MetaDataTestCase(unittest.TestCase):
         return self.engine.connect()
 
     def test_create_all(self):
-        """Create all tables in the schema using the metadata object.
+        """Create all tables in the schema using the metadata object and a
+        sqlite connection.
 
-        Check that the reflected `MetaData` from the database matches the
-        `MetaData` created from the schema.
+        Check that the reflected `MetaData` from the database matches that
+        which was created by the `MetaDataBuilder`.
         """
+
+        def _sorted_indexes(indexes: set[Index]) -> list[Index]:
+            """Return a sorted list of indexes."""
+            return sorted(indexes, key=lambda i: i.name)
+
+        def _sorted_constraints(constraints: set[Constraint]) -> list[Constraint]:
+            """Return a sorted list of constraints with the
+            `PrimaryKeyConstraint` objects filtered out.
+            """
+            return sorted(
+                [c for c in constraints if not isinstance(c, PrimaryKeyConstraint)], key=lambda c: c.name
+            )
+
         with self.connection() as connection:
             schema = Schema.model_validate(self.yaml_data)
             schema.name = "main"
@@ -77,8 +100,50 @@ class MetaDataTestCase(unittest.TestCase):
                     self.assertEqual(type(md_column.type), type(md_db_column.type))
                     self.assertEqual(md_column.nullable, md_db_column.nullable)
                     self.assertEqual(md_column.primary_key, md_db_column.primary_key)
+                self.assertTrue(
+                    (md_table.constraints and md_db_table.constraints)
+                    or (not md_table.constraints and not md_table.constraints),
+                    "Constraints not created correctly",
+                )
+                if md_table.constraints:
+                    self.assertEqual(len(md_table.constraints), len(md_db_table.constraints))
+                    md_constraints = _sorted_constraints(md_table.constraints)
+                    md_db_constraints = _sorted_constraints(md_db_table.constraints)
+                    for md_constraint, md_db_constraint in zip(md_constraints, md_db_constraints):
+                        self.assertEqual(md_constraint.name, md_db_constraint.name)
+                        self.assertEqual(md_constraint.deferrable, md_db_constraint.deferrable)
+                        self.assertEqual(md_constraint.initially, md_db_constraint.initially)
+                        if isinstance(md_constraint, ForeignKeyConstraint):
+                            md_fk: ForeignKeyConstraint = md_constraint
+                            md_db_fk: ForeignKeyConstraint = md_db_constraint
+                            self.assertEqual(md_fk.referred_table.name, md_db_fk.referred_table.name)
+                            self.assertEqual(md_fk.column_keys, md_db_fk.column_keys)
+                        elif isinstance(md_constraint, UniqueConstraint):
+                            md_uniq: UniqueConstraint = md_constraint
+                            md_db_uniq: UniqueConstraint = md_db_constraint
+                            self.assertEqual(md_uniq.columns.keys(), md_db_uniq.columns.keys())
+                        elif isinstance(md_constraint, CheckConstraint):
+                            md_check: CheckConstraint = md_constraint
+                            md_db_check: CheckConstraint = md_db_constraint
+                            self.assertEqual(str(md_check.sqltext), str(md_db_check.sqltext))
+                self.assertTrue(
+                    (md_table.indexes and md_db_table.indexes)
+                    or (not md_table.indexes and not md_table.indexes),
+                    "Indexes not created correctly",
+                )
+                if md_table.indexes:
+                    md_indexes = _sorted_indexes(md_table.indexes)
+                    md_db_indexes = _sorted_indexes(md_db_table.indexes)
+                    self.assertEqual(len(md_indexes), len(md_db_indexes))
+                    for md_index, md_db_index in zip(md_table.indexes, md_db_table.indexes):
+                        self.assertEqual(md_index.name, md_db_index.name)
+                        self.assertEqual(md_index.columns.keys(), md_db_index.columns.keys())
 
-            # TODO: Check constraints and indexes
+    def test_schema_metadata(self):
+        """Test that the `MetaData` object created from the schema is
+        correct.
+        """
+        pass
 
 
 if __name__ == "__main__":
