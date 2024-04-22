5973ddf4d2bb0deb8f74a6971f791f662e83f17f
Jeremy McCormick <jeremym@slac.stanford.edu>
2024-04-22 12:47:36 -0500

    Update sphinx configuration to use Documenteer guide
    https://documenteer.lsst.io/guides/configuration.html

--- docs/conf.py
+++ docs/conf.py
@@ -3,11 +3,4 @@
 This configuration only affects single-package Sphinx documenation builds.
 """
 
-from documenteer.conf.pipelinespkg import *  # noqa: F403, import *
-
-project = "felis"
-html_theme_options["logotext"] = project  # noqa: F405, unknown name
-html_title = project
-html_short_title = project
-doxylink = {}
-exclude_patterns = ["changes/*"]
+from documenteer.conf.guide import *  # noqa: F403
