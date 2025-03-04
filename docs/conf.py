39604355ebee0f73f86e40e306517e1c6dac8b51
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-03-04 14:18:55 -0600

    Exclude model_post_init in autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -10,4 +10,6 @@ autodoc_pydantic_settings_show_config_summary = False
 autodoc_pydantic_settings_show_json = False
 autodoc_pydantic_model_show_json = False
 
+autodoc_default_options = {"exclude-members": "model_post_init"}
+
 exclude_patterns = ["changes/*"]
